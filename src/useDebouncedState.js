bea30a68802b0291fba5a2ab9fe778230ef025b1
Naman Muley <naman.g.muley@gmail.com>
2025-09-27 00:51:23 +0530

    Add src/useDebouncedState.js

---
+++ src/useDebouncedState.js
@@ -0,0 +1,15 @@
+// useDebouncedState.js
+import { useState, useEffect } from 'react';
+
+function useDebouncedState(value, delay) {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debounced;
+}
+
+export default useDebouncedState;
